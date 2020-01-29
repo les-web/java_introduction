38bf106eaa6013a15ac3007759d1429c812cef9f
Leszek Żebrowski <leszek-zebrowski@o2.pl>
2020-01-29 15:39:08 +0100

    Other simple types used + homework 2.

--- src/main/java/introduction/SimpleTypes.java
+++ src/main/java/introduction/SimpleTypes.java
@@ -110,33 +110,7 @@ public class SimpleTypes {
 
 
 
-        /*
 
-        Boxed primitives
-        Parę razy pewnie widziałeś już np. klasę Integer, Long etc.
-        Są to tzw . boxed primitives, czyli opakowania (klasy) na typy prymitywne.
-        Dlaczego są one potrzebne zostanie wyjaśnione później (np. w kolekcjach).
-        Zostały one także użyte w poprzednim zadaniu.
-        Wykonaj następujący kod:
-
-        */
-/*
-
-        public class App {
-            public static void main(String[] args) {
-               Integer intValBoxed = 10;
-               Long longValBoxed = 10L;
-               Short shortValBoxed = 10;
-               Double doubleValBoxed = 10.0;
-               Float floatValBoxed = 10.0F;
-            }
-        }
-  */
-         /*
-        Czy zostały zwrócone jakieś błędy?
-        Poszukaj w internecie, dlaczego tak się dzieje.
-        Słowa kluczowe: java autoboxing.
-       */
 
     }
 }
