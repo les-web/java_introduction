29baa2dce80dfb7a022a38a7201a19ba01888778
Leszek Żebrowski <leszek-zebrowski@o2.pl>
2020-02-01 10:01:49 +0100

    Exercises with strings.

--- src/main/java/Strings.java
+++ src/main/java/Strings.java
@@ -1,6 +1,12 @@
 public class Strings {
 
     public static void main(String[] args) {
+
         String text = "Ala ma kota";
+        String text2 = "Ala ma kota";
+
+        System.out.println("Text1: " + text + ", text2: " + text2);
+        System.out.println( text == text2);
+        System.out.println("Czy obie zmienne to to samo ? " + text.equals(text2));
     }
 }
