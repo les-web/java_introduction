89e7fa855f9c4fb45ea62ab8ade08946999fce99
Leszek Żebrowski <leszek-zebrowski@o2.pl>
2020-02-26 19:16:59 +0100

    Homework 20200219. Roman to decimal. Zmiany w Chessboard.

---
+++ src/main/java/classes/basics/homework20200205/MyTests.java
@@ -0,0 +1,56 @@
+package classes.basics.homework20200205;
+
+import java.sql.Array;
+import java.util.Arrays;
+
+public class MyTests {
+    public static void main(String[] args) {
+        // jak dzialaja metody z Arrays
+        int[] a = {10, 1, 3, 9, 5, 4, 7, 8, 9, 2};
+        System.out.println(" Array 1d a [], length =  " + a.length);
+        System.out.println(Arrays.toString(a));
+        int[][] b = {
+                {1, 2},
+                {3, 4},
+                {5, 6},
+                {7, 8},
+                {9, 10}
+
+        };
+        System.out.println(" Array 2d b [][], length =  " + b.length);
+        for (int i = 0; i < b.length; i++) {
+            System.out.println(Arrays.toString(b[i]));
+
+        }
+        // test is a 3d array
+        int[][][] test = {
+                {
+                        {1, 12, 13},
+                        {2, 21, 22}
+                },
+                {
+                        {3, 31, 32, 33},
+                        {4},
+                        {5, 51}
+                }
+        };
+        // for..each loop to iterate through elements of 3d array
+        System.out.println(" Array 3d test [][][], length = " + test.length);
+
+        for (int[][] array2D : test) {
+            for (int[] array1D : array2D) {
+//                for(int item: array1D) {
+//                    System.out.print(item + " ");
+//                }
+                System.out.println(Arrays.toString(array1D));
+
+            }
+        }
+        //      System.out.println(test[1][1][0]);
+        Arrays.parallelSort(a);
+        System.out.println(" Array 1d a [], sorted  ");
+        System.out.println(Arrays.toString(a));
+
+
+    }
+}
